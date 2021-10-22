c1f7c855c8287465fe77cf4de8aac24b70381635
LuisMiRey <74019851+LuisMiRey@users.noreply.github.com>
2021-10-22 11:20:34 +0200

    Create Notes.py
    First commit - Link about MarkDown Cheatsheet

---
+++ Notes.py
@@ -0,0 +1,4 @@
+# Importan notes about Certificate 
+# 
+# The next link above is about Markdown Cheatsheet useful for Jupyter Notebook:
+# https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
