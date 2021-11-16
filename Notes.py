6909d3b0095ce889546a1f3c1891ee2cb29d491f
LuisMiRey <74019851+LuisMiRey@users.noreply.github.com>
2021-11-16 13:02:45 +0100

    Update Notes.py
    added new links about 4 course of the certificate.

--- Notes.py
+++ Notes.py
@@ -2,3 +2,12 @@
 # 
 # The next link above is about Markdown Cheatsheet useful for Jupyter Notebook:
 # https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet
+#
+#
+# Link about Exception. There are many more exceptions that are built into Python. 
+# https://docs.python.org/3/library/exceptions.html 
+#
+# Link about Quick Reference Sheet 
+# https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%203/Python_reference_sheet.pdf?utm_medium=Exinfluencer&utm_source=Exinfluencer&utm_content=000026UJ&utm_term=10006555&utm_id=NA-SkillsNetwork-Channel-SkillsNetworkCoursesIBMDeveloperSkillsNetworkPY0101ENSkillsNetwork19487395-2021-01-01
+#
+#
